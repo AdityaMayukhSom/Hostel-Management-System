c2d98a6a0b5ac1a100db5a6eb8e70fa47168641f
Aditya Mayukh Som <adi.kg2@gmail.com>
2023-05-06 20:32:17 +0530

    pug templates added

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import { port } from './controllers/environment';
 import path from 'path';
 import cors from 'cors';
 import express from 'express';
-import bodyParser from 'body-parser';
 
 // this is the root directory path for the project
 const rootDir = path.resolve(__dirname, '..')
@@ -11,7 +10,7 @@ const rootDir = path.resolve(__dirname, '..')
 const app = express();
 
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(express.static(path.join(rootDir, "public")))
 
 app.set('views', path.join(rootDir, "views"));
@@ -20,6 +19,22 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
     res.status(200).render('index')
 })
+app.get('/admin-login', (req, res) => {
+    res.status(200).render('admin_login')
+})
+app.get('/application-form', (req, res) => {
+    res.status(200).render('application_form')
+})
+app.get('/new-admin', (req, res) => {
+    res.status(200).render('new_admin')
+})
+app.get('/student-login', (req, res) => {
+    res.status(200).render('student_login')
+})
+app.get('/visitors', (req, res) => {
+    res.status(200).render('visitors')
+})
+
 
 app.listen(port, function () {
     console.log('⚡server running on port ' + port + ' ⚡');
